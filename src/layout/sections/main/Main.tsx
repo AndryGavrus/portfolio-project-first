868b59902cc4ad9b259ba61c93e3bb1764362a86
Andrey <andry.gavrus@gmail.com>
2024-10-06 13:37:02 +0300

    Add ParalaxTilt

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -4,6 +4,7 @@ import { FlexWrapper } from '../../../components/FlexWrapper';
 import { Container } from '../../../components/Container';
 import { S } from './Main_Styles';
 import Typewriter from 'typewriter-effect';
+import Tilt from 'react-parallax-tilt';
 
 export const Main: React.FC = () => {
     return (
@@ -13,7 +14,6 @@ export const Main: React.FC = () => {
                     <div>
                         <S.SmallText>Hi There</S.SmallText>
                         <S.Name>I am <span>Andrey Gavrus</span></S.Name>
-                        {/* <S.Maintitle>A Web Developer.</S.Maintitle> */}
                         <S.Maintitle>
                             <p>A Web Developer.</p>
                             <Typewriter
@@ -25,9 +25,11 @@ export const Main: React.FC = () => {
                             />
                         </S.Maintitle>
                     </div>
-                    <S.PhotoWrapper>
-                        <S.Photo src={photo} />
-                    </S.PhotoWrapper>
+                    <Tilt>
+                        <S.PhotoWrapper>
+                            <S.Photo src={photo} />
+                        </S.PhotoWrapper>
+                    </Tilt>
                 </FlexWrapper>
             </Container>
         </S.Main>
